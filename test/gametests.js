e01a63327d2e4d7bcd93725c3149c95168b78bca
Ethan Padden <ethanpadden@gmail.com>
2021-01-30 18:29:34 +0000

    Add vertical winning move basic tests

--- test/gametests.js
+++ test/gametests.js
@@ -73,4 +73,61 @@ describe('Horizontal wins', function () {
         });
     });
     
+});
+
+describe('Vertical wins', function () {
+    describe('Vertical win in row 2', function () {
+        it('Player 1 should win the game', function () {
+            var game = new Game();
+            var player1 = new Player(null, 'John');
+            var player2 = new Player(null, 'Mary');
+    
+            game.addPlayer(player1);
+            game.addPlayer(player2);
+    
+            for (var i = 1; i <= 4; i++) {
+                result = game.makeMove(1, player1.number);
+                assert.equal(result, 0);
+    
+                result = game.makeMove(2, player2.number);
+                assert.equal(result, 0);
+            }
+
+    
+            var result = game.makeMove(1, player1.number);
+            assert.equal(result, 1);
+        });
+    });
+    
+    describe('Vertical win in top row', function () {
+        it('Player 1 should win the game', function () {
+            var game = new Game();
+            var player1 = new Player(null, 'John');
+            var player2 = new Player(null, 'Mary');
+    
+            game.addPlayer(player1);
+            game.addPlayer(player2);
+    
+            var result;
+
+            result = game.makeMove(3, player1.number);
+            assert.equal(result, 0);
+
+    
+            for (var i = 1; i <= 4; i++) {
+                result = game.makeMove(2, player2.number);
+                assert.equal(result, 0);
+    
+                result = game.makeMove(1, player1.number);
+                assert.equal(result, 0);
+            }
+    
+            result = game.makeMove(3, player2.number);
+            assert.equal(result, 0);
+
+            result = game.makeMove(1, player1.number);
+            assert.equal(result, 1);
+        });
+    });
+    
 })
