94c7f30722da8fbaf096f7b227f86850644dc4c1
Ethan Padden <ethanpadden@gmail.com>
2021-01-30 19:36:51 +0000

    Add tests to check winning move vertically with a positive slope

--- test/gametests.js
+++ test/gametests.js
@@ -131,3 +131,53 @@ describe('Vertical wins', function () {
     });
     
 })
+
+
+describe('Diagonal wins - positive slope', function () {
+    describe('Diagonal win from bottom left corner', function () {
+        it('Player 1 should win the game', function () {
+            var game = new Game();
+            var player1 = new Player(null, 'John');
+            var player2 = new Player(null, 'Mary');
+    
+            var result;
+            game.addPlayer(player1);
+            game.addPlayer(player2);
+    
+            result = game.makeMove(1, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(2, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(2, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(3, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(3, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(4, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(3, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(4, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(4, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(5, player2.number);
+            console.log(game.board.toString());
+            assert.equal(result, 0);
+            result = game.makeMove(4, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(5, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(5, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(5, player2.number);
+            assert.equal(result, 0);
+
+            result = game.makeMove(5, player1.number);
+            assert.equal(result, 1);
+        });
+    });
+    
+    
+});
