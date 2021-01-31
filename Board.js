23fa2d63a290e6c178a646fcb0a61c1d20e2a107
Ethan Padden <ethanpadden@gmail.com>
2021-01-31 13:09:07 +0000

    Add descriptive comments to Board and Game classes and format/clean code

--- Board.js
+++ Board.js
@@ -1,6 +1,5 @@
 const emptySqaure = '[ ]';
-
-// Index 0 for player 1, index 1 for player 2
+// Game pieces for player 1 and 2 respectively
 const gamePieces = ['[X]', '[O]'];
 
 class Board {
@@ -9,6 +8,7 @@ class Board {
 
         // Build up an empty board of 6 empty rows
         for (var i = 0; i < 6; i++) {
+            // For 6 rows, push an array of 9 squares
             this.board.push(
                 new Array(
                     emptySqaure,
@@ -25,30 +25,45 @@ class Board {
         }
     }
 
+    /**
+     * Override toString method
+     */
     toString() {
+        // String to be returned
         var boardStr = '';
 
-        // The board is an array of rows
+        // For every row in the board
         for (var i = 0; i < this.board.length; i++) {
+            // Each row is an array, so a string is build for each
             var rowStr = '';
             var rowArr = this.board[i];
+
+            // For each sqare in the row, append to the row string
             for (var j = 0; j < rowArr.length; j++) {
                 rowStr += rowArr[j];
             }
 
+            // Append the row string to the board string on a new line
             boardStr += rowStr + '\n';
         }
 
         return boardStr;
     }
 
-    // Not zero indexed
+    /**
+     * Checks if a particular sqaure is empty (the character is the empty sqaure character)
+     * @param {*} row : The row number (integer in the valid range, not zero-indexed) to check
+     * @param {*} column : The column number (integer in the valid range, not zero-indexed) to check
+     */
     sqaureisEmpty(row, column) {
         var row = this.board[row - 1];
         return row[column - 1] === emptySqaure;
     }
 
-    // Input is assumed to be valid (an integer in the correct range, not zero indexed)
+    /**
+     * Returns the (non zero-indexed) row number for the next empty space in a particular column
+     * @param {*} column : The column number (integer in the valid range - where the column is not full, not zero-indexed)
+     */
     nextEmptySquareInColumn(column) {
         // Start from the bottom of the board, and return the (non zero-indexed) row number
         for (var i = this.board.length - 1; i >= 0; i--) {
@@ -56,117 +71,204 @@ class Board {
         }
     }
 
-    // Assumes that the square is empty
-    // Arguments are not zero indexed
-    // Rows start from the top row at 1
-    // Columns start from the left at 1
-    // playerNumber is either 1 or 2
+    /**
+     * Sets the sqaure at a particular row and column to be the game piece of the player who's number is in the
+     * This assumes that the square is empty, otherwise it will override the piece that is already there
+     * (i.e. it must be verified that the square is empty before calling this method)
+     * @param {*} row : The column number (integer in the valid range, not zero-indexed)
+     * @param {*} column : The column number (integer in the valid range, not zero-indexed)
+     * @param {*} playerNumber : The player number who's piece is to be placed (1 or 2)
+     */
     addGamePiece(row, column, playerNumber) {
-        // console.log('PLYAER: ' + playerNumber);
+        // Get the row as an array
         var rowArr = this.board[row - 1];
-        // console.log(row);
-        // console.log(rowArr);
+
+        // Set the square at the correct column number to be the player's piece
         rowArr[column - 1] = gamePieces[playerNumber - 1];
 
-        // console.log(rowArr);
-
+        // Set this updated row back in the board
         this.board[row - 1] = rowArr;
-        // console.log(this.board);
     }
 
+    /**
+     * Calls 4 different methods corresponding to checking the 4 different ways a player can win
+     * Checks if the move was a winning move
+     * @param {*} row : The row number (integer in the valid range, not zero-indexed)
+     * @param {*} column : The column number (integer in the valid range, not zero-indexed)
+     * @param {*} playerNumber : The player number who made the move (1 or 2)
+     * @returns true if at least one of the conditions for a win are met
+     */
     checkForWinner(row, column, playerNumber) {
         var horizontalWin = this.checkForWinnerHorizontally(row, playerNumber);
         var verticalWin = this.checkForWinnerVertically(column, playerNumber);
-        var diagonalWinPositiveSlope = this.checkForWinnerDiagonallyPositiveSlope(row, column, playerNumber);
-        var diagonalWinNegativeSlope = this.checkForWinnerDiagonallyNegativeSlope(row, column, playerNumber);
-        return horizontalWin || verticalWin || diagonalWinPositiveSlope || diagonalWinNegativeSlope;
+        var diagonalWinPositiveSlope = this.checkForWinnerDiagonallyPositiveSlope(
+            row,
+            column,
+            playerNumber
+        );
+        var diagonalWinNegativeSlope = this.checkForWinnerDiagonallyNegativeSlope(
+            row,
+            column,
+            playerNumber
+        );
+        return (
+            horizontalWin ||
+            verticalWin ||
+            diagonalWinPositiveSlope ||
+            diagonalWinNegativeSlope
+        );
     }
 
+    /**
+     * Checks if the player specified by the player number has 5 pieces in a row in the specified row
+     * @param {*} row : The row number (integer in the valid range, not zero-indexed)
+     * @param {*} playerNumber : The player whose piece to check (1 or 2)
+     */
     checkForWinnerHorizontally(row, playerNumber) {
+        // Get the row in question
         var rowArr = this.board[row - 1];
+
+        // Before checking, assume that the player has not won
+        // This will be set to true if we find that they have
         var hasWon = false;
 
-        // FIND THE FIRST PLAYER PIECE IN THE ROW:
-        // Start at the beginning of the row
+        // This is the piece to check against
         var targetPiece = gamePieces[playerNumber - 1];
-        var i = 0;
-        while (i < 5) {
-            if (rowArr[i] == targetPiece) {
+
+        // Start the pointer at the beggining of the row
+        var pointer = 0;
+
+        // Iterate up to the 5th column
+        while (pointer < 5) {
+            // If we find a piece, stop iterating
+            if (rowArr[pointer] == targetPiece) {
                 break;
             }
-            i++;
+
+            // Otherwise, continue
+            pointer++;
         }
 
         // If the player's piece was not found in the first 5 columns,
-        // they have not won in that row
-        if (i == 5) return hasWon;
+        // they cannot have won in that row
+        // (because there are only 4 columns left)
+        if (pointer == 5) return hasWon;
 
+        // Otherwise, there is a possibility that they have
+        // At this point, pointer != 5, so we must have found a piece in the current column
+        // So far we have found 1 in a row
         var inARow = 1;
-        i++;
 
-        while (i < rowArr.length) {
-            if (rowArr[i] == targetPiece) {
+        // Move onto the next column
+        pointer++;
+
+        // Iterate to the end
+        while (pointer < rowArr.length) {
+            // If we find a piece, increase inARow
+            if (rowArr[pointer] == targetPiece) {
                 inARow++;
             } else {
                 // Reset and continue for next column
                 inARow = 0;
             }
-            i++;
-            if (inARow == 5) hasWon = true;
+            pointer++;
+
+            // If at any point we find 5 in a row, stop iterating as the player has won
+            if (inARow == 5) {
+                hasWon = true;
+                break;
+            }
         }
 
         return hasWon;
     }
 
+    /**
+     * Checks if the player specified by the player number has 5 pieces in a row in the specified column
+     * @param {*} column : The column number (integer in the valid range, not zero-indexed)
+     * @param {*} playerNumber : The player whose piece to check (1 or 2)
+     */
     checkForWinnerVertically(column, playerNumber) {
+        // Before checking, assume that the player has won
+        // This will be set to false if we find that they have not
         var hasWon = true;
 
-        // For the player to win vertically, there are 2 cases
-        var i = 1;
-        var rowArr = this.board[i - 1];
+        // Start the pointer at the first row
+        var pointer = 1;
+        var rowArr = this.board[pointer - 1];
+
+        // This is the piece to check against
         var targetPiece = gamePieces[playerNumber - 1];
 
-        // If the target piece is  in row 1, check rows 1 to 5
+        // For the player to win vertically, there are 2 cases:
+        // If the target piece is in row 1, check rows 1 to 5
         // If not, check rows 2 to 6
         if (rowArr[column] != targetPiece) {
-            i++;
+            pointer++;
         }
 
-        while (i < this.board.length) {
-            rowArr = this.board[i - 1];
+        // Iterate to the end of the column
+        while (pointer < this.board.length) {
+            // Get the row array at that point
+            rowArr = this.board[pointer - 1];
 
+            // Check in the column for the target piece
             if (rowArr[column - 1] !== targetPiece) {
+                // If at any point, we find that the player's piece is not there,
+                // they have not won, so stop iterating and return that they have not
                 hasWon = false;
                 break;
             }
-            i++;
+
+            // Otherwise, check the next row
+            pointer++;
         }
 
+        // If we have found the player's piece in every row that we were looking in,
+        // return that the player has won
         return hasWon;
     }
 
+    /**
+     * Checks if the player specified by the player number has 5 pieces in a row,
+     * in the diagonal that runs from the bottom left of the board
+     * to the top right of the board that passes through where the piece was added
+     * @param {*} row : The row number (integer in the valid range, not zero-indexed)
+     * @param {*} column : The column number (integer in the valid range, not zero-indexed)
+     * @param {*} playerNumber : The player whose piece to check (1 or 2)
+     */
     checkForWinnerDiagonallyPositiveSlope(row, column, playerNumber) {
+        // Before checking, assume that the player has not won
+        // This will be set to true if we find that they have
         var hasWon = false;
 
-        // Start at the piece's position
-        var currentRow = row+1;
-        var currentColumn = column-1;
+        // Start at the piece's position,
+        // and move 1 square down and left
+        var currentRow = row + 1;
+        var currentColumn = column - 1;
+
+        // This is the piece to check against
         var targetPiece = gamePieces[playerNumber - 1];
 
-        // This represents the piece that was just added
+        // We know that there is 1 in a row,
+        // at the square where the piece was just added
         var inARow = 1;
 
-        // Move down and left
+        /** Moving down and left */
         // as we move down, the row pointer increases
         // as we move left, the column pointer decreases
-        // These limits are not zero indexed
+        // the limits in the while loop condition are not zero indexed
 
+        // Iterate to the bottom left of the board
         while (currentRow <= 6 && currentColumn >= 1) {
-        
-
-            if (this.board[currentRow-1][currentColumn-1] == targetPiece) {
+            // If we find the player's piece here
+            if (this.board[currentRow - 1][currentColumn - 1] == targetPiece) {
+                // Increment inARow
                 inARow++;
+
+                // If at any point we have found 5 in a row
                 if (inARow == 5) {
+                    // stop iterating and set that the player has won
                     hasWon = true;
                     break;
                 }
@@ -180,20 +282,26 @@ class Board {
             currentColumn--;
         }
 
-        // Reset the pointers back to the piece that was just added
-        var currentRow = row-1;
-        var currentColumn = column+1;
+        // Reset the pointers back to the piece that was just added,
+        // and move 1 square up and right
+        var currentRow = row - 1;
+        var currentColumn = column + 1;
 
-        // Move up and right
+        /** Moving up and right */
         // as we move up, the row pointer decreases
         // as we move right, the column pointer increases
-        // These limits are not zero indexed
-        while (currentRow >= 1 && currentColumn <= 9) {
-            
+        // the limits in the while loop condition are not zero indexed
 
-            if (this.board[currentRow-1][currentColumn-1] == targetPiece) {
+        // Iterate to the top right of the board
+        while (currentRow >= 1 && currentColumn <= 9) {
+            // If we find the player's piece here
+            if (this.board[currentRow - 1][currentColumn - 1] == targetPiece) {
+                // Increment inARow
                 inARow++;
+
+                // If at any point we have found 5 in a row
                 if (inARow == 5) {
+                    // stop iterating and set that the player has won
                     hasWon = true;
                     break;
                 }
@@ -205,33 +313,52 @@ class Board {
             // Move up and right 1 square
             currentRow--;
             currentColumn++;
-        } 
+        }
 
+        // Return whether or not the player has won
         return hasWon;
     }
 
+    /**
+     * Checks if the player specified by the player number has 5 pieces in a row,
+     * in the diagonal that runs from the top left of the board
+     * to the bottom right of the board that passes through where the piece was added
+     * @param {*} row : The row number (integer in the valid range, not zero-indexed)
+     * @param {*} column : The column number (integer in the valid range, not zero-indexed)
+     * @param {*} playerNumber : The player whose piece to check (1 or 2)
+     */
     checkForWinnerDiagonallyNegativeSlope(row, column, playerNumber) {
+        // Before checking, assume that the player has not won
+        // This will be set to true if we find that they have
         var hasWon = false;
 
         // Start at the piece's position
-        var currentRow = row-1;
-        var currentColumn = column+1;
+        // and move 1 square up and left
+        var currentRow = row - 1;
+        var currentColumn = column - 1;
+
+        // This is the piece to check against
         var targetPiece = gamePieces[playerNumber - 1];
 
-        // This represents the piece that was just added
+        // We know that there is 1 in a row,
+        // at the square where the piece was just added
         var inARow = 1;
 
-        // Move up and left
+        /** Moving up and left */
         // as we move up, the row pointer decreases
         // as we move left, the column pointer decreases
-        // These limits are not zero indexed
+        // the limits in the while loop condition are not zero indexed
 
+        // Iterate to the top left of the board
         while (currentRow >= 1 && currentColumn >= 1) {
-        
-
-            if (this.board[currentRow-1][currentColumn-1] == targetPiece) {
+            // If we find the player's piece here
+            if (this.board[currentRow - 1][currentColumn - 1] == targetPiece) {
+                // Increment inARow
                 inARow++;
+
+                // If at any point we have found 5 in a row
                 if (inARow == 5) {
+                    // stop iterating and set that the player has won
                     hasWon = true;
                     break;
                 }
@@ -240,25 +367,31 @@ class Board {
                 break;
             }
 
-            // Move down and left 1 square
+            // Move up and left 1 square
             currentRow--;
             currentColumn--;
         }
 
-        // Reset the pointers back to the piece that was just added
-        var currentRow = row+1;
-        var currentColumn = column+1;
+        // Reset the pointers back to the piece that was just added,
+        // and move 1 square down and right
+        var currentRow = row + 1;
+        var currentColumn = column + 1;
 
-        // Move down and right
+        /** Moving down and right */
         // as we move down, the row pointer increases
         // as we move right, the column pointer increases
-        // These limits are not zero indexed
-        while (currentRow <= 6 && currentColumn <= 9) {
-            
+        // the limits in the while loop condition are not zero indexed
 
-            if (this.board[currentRow-1][currentColumn-1] == targetPiece) {
+        // Iterate to the bottom right of the board
+        while (currentRow <= 6 && currentColumn <= 9) {
+            // If we find the player's piece here
+            if (this.board[currentRow - 1][currentColumn - 1] == targetPiece) {
+                // Increment inARow
                 inARow++;
+
+                // If at any point we have found 5 in a row
                 if (inARow == 5) {
+                    // stop iterating and set that the player has won
                     hasWon = true;
                     break;
                 }
@@ -267,11 +400,12 @@ class Board {
                 break;
             }
 
-            // Move up and right 1 square
+            // Move down and right 1 square
             currentRow++;
             currentColumn++;
-        } 
+        }
 
+        // Return whether or not the player has won
         return hasWon;
     }
 }
--- Game.js
+++ Game.js
@@ -1,4 +1,3 @@
-const Player = require('./Player.js');
 const Board = require('./Board.js');
 
 class Game {
@@ -7,32 +6,57 @@ class Game {
         this.board = new Board();
     }
 
+    /**
+     * Adds a player to the game
+     * @param {*} player : the Player object to be added to the game
+     * @returns : true if the player was added succesfully, false if the game is full
+     */
     addPlayer(player) {
         if (typeof this.player1 == 'undefined') {
+            // If there is no player 1 yet, set this player to be player 1
             this.player1 = player;
             this.player1.setPlayerNumber(1);
+
+            // The player was added successfully
             return true;
         } else if (typeof this.player2 == 'undefined') {
+            // If there is a player 1 already, set this player to be player 2
             this.player2 = player;
-            this.startGame();
             this.player2.setPlayerNumber(2);
+
+            // The game is now full and can be started
+            this.startGame();
+
             return true;
         } else {
+            // The game is already full, so the player was not added successfully
             return false;
         }
     }
 
+    /**
+     * Sets the game state to active
+     * Specifies that it is player 1's turn
+     */
     startGame() {
         this.active = true;
         this.turn = this.player1;
     }
 
+    /**
+     * Check who's turn it is currently
+     * Sets the turn to the other player
+     */
     nextTurn() {
         if (this.turn === this.player1) this.turn = this.player2;
         else this.turn = this.player1;
     }
 
-    // Assumes the position is a string value passed from the client
+    /**
+     * Checks whether an input position to the game is a valid move
+     * @param {*} positionStr : a string representation of the position (1-9)
+     * @returns : true if the move is valid (conditions specified in method body)
+     */
     isValidMove(positionStr) {
         if (isNaN(positionStr)) {
             // Check if the string is a number
@@ -43,7 +67,6 @@ class Game {
             if (!Number.isInteger(positionFloat)) {
                 // Check if the string is an integer
                 console.log('Position is not an integer');
-
                 return false;
             } else {
                 // Convert the float to an integer
@@ -52,13 +75,11 @@ class Game {
                 if (positionFloat < 1 || position > 9) {
                     // Check if the position is in the correct range
                     console.log('Position is not in the correct range');
-
                     return false;
                 } else {
-                    // Check if the top square of the column is empty
                     if (!this.board.sqaureisEmpty(1, position)) {
+                        // Check if the top square of the column is empty
                         console.log('No room left in column');
-
                         return false;
                     }
                 }
@@ -67,24 +88,38 @@ class Game {
         return true;
     }
 
-    // Returns true if move was successful
-    // false if the move was invalid
+    /**
+     * Adds a game piece into a specified column as a move by the specified player
+     * @param {*} column : the column number to place the piece in (not zero-indexed)
+     * @param {*} playerNumber : the player number who is making the move (1 or 2)
+     * @returns true if move was successful, false if the move was invalid
+     */
     makeMove(column, playerNumber) {
         if (this.isValidMove(column)) {
+            // If the move is valid
+            // Add a game piece to the next empty sqare of the board in that column
             var row = this.board.nextEmptySquareInColumn(column);
             this.board.addGamePiece(row, column, playerNumber);
+
+            // Check was this a winning move
             var hasWon = this.board.checkForWinner(row, column, playerNumber);
-            if(hasWon) {
+
+            if (hasWon) {
+                // If it was a winning move:
+                // Set the game as inactive
+                this.active = false;
+                // Return a signal to show it was a winning move
                 return 1;
             } else {
+                // If it was not a winning move:
+                // Return a signal to show it was not winning move, but was successful (valid and executed)
                 return 0;
             }
         } else {
+            // Signal that the move was invalid
             return -1;
         }
     }
-
-
 }
 
 module.exports = Game;
