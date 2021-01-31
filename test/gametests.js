0be07246183bfed42ddca0176fbc123285e42d88
Ethan Padden <ethanpadden@gmail.com>
2021-01-31 13:20:06 +0000

    Add diagonal win tests from middle of board

--- test/gametests.js
+++ test/gametests.js
@@ -11,6 +11,8 @@ describe('Horizontal wins', function () {
     
             game.addPlayer(player1);
             game.addPlayer(player2);
+
+            var result;
     
             for (var i = 1; i <= 4; i++) {
                 result = game.makeMove(i, player1.number);
@@ -20,7 +22,7 @@ describe('Horizontal wins', function () {
                 assert.equal(result, 0);
             }
     
-            var result = game.makeMove(5, player1.number);
+            result = game.makeMove(5, player1.number);
             assert.equal(result, 1);
         });
     });
@@ -45,7 +47,7 @@ describe('Horizontal wins', function () {
     
             }
     
-            var result = game.makeMove(9, player1.number);
+            result = game.makeMove(9, player1.number);
             assert.equal(result, 1);
         });
     });
@@ -58,6 +60,8 @@ describe('Horizontal wins', function () {
     
             game.addPlayer(player1);
             game.addPlayer(player2);
+
+            var result;
     
             for (var i = 3; i <= 6; i++) {
                 result = game.makeMove(i, player1.number);
@@ -68,7 +72,7 @@ describe('Horizontal wins', function () {
     
             }
     
-            var result = game.makeMove(7, player1.number);
+            result = game.makeMove(7, player1.number);
             assert.equal(result, 1);
         });
     });
@@ -84,6 +88,8 @@ describe('Vertical wins', function () {
     
             game.addPlayer(player1);
             game.addPlayer(player2);
+
+            var result;
     
             for (var i = 1; i <= 4; i++) {
                 result = game.makeMove(1, player1.number);
@@ -94,7 +100,7 @@ describe('Vertical wins', function () {
             }
 
     
-            var result = game.makeMove(1, player1.number);
+            result = game.makeMove(1, player1.number);
             assert.equal(result, 1);
         });
     });
@@ -163,7 +169,6 @@ describe('Diagonal wins - positive slope', function () {
             result = game.makeMove(4, player1.number);
             assert.equal(result, 0);
             result = game.makeMove(5, player2.number);
-            console.log(game.board.toString());
             assert.equal(result, 0);
             result = game.makeMove(4, player1.number);
             assert.equal(result, 0);
@@ -178,8 +183,50 @@ describe('Diagonal wins - positive slope', function () {
             assert.equal(result, 1);
         });
     });
+
+    describe('Diagonal win from column 5 to last column', function () {
+        it('Player 1 should win the game', function () {
+            var game = new Game();
+            var player1 = new Player(null, 'John');
+            var player2 = new Player(null, 'Mary');
     
+            var result;
+            game.addPlayer(player1);
+            game.addPlayer(player2);
     
+            result = game.makeMove(5, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(6, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(6, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(7, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(7, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(8, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(7, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(8, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(8, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(9, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(8, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(9, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(9, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(9, player2.number);
+            assert.equal(result, 0);
+
+            result = game.makeMove(9, player1.number);
+            assert.equal(result, 1);
+        });
+    });
 });
 
 
@@ -229,5 +276,47 @@ describe('Diagonal wins - negative slope', function () {
         });
     });
     
+    describe('Diagonal win from column 5 to first column', function () {
+        it('Player 1 should win the game', function () {
+            var game = new Game();
+            var player1 = new Player(null, 'John');
+            var player2 = new Player(null, 'Mary');
     
+            var result;
+            game.addPlayer(player1);
+            game.addPlayer(player2);
+    
+            result = game.makeMove(5, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(4, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(4, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(3, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(3, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(2, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(3, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(2, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(2, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(1, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(2, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(1, player2.number);
+            assert.equal(result, 0);
+            result = game.makeMove(1, player1.number);
+            assert.equal(result, 0);
+            result = game.makeMove(1, player2.number);
+            assert.equal(result, 0);
+
+            result = game.makeMove(1, player1.number);
+            assert.equal(result, 1);
+        });
+    });
 });
